294583952872aeb0a67b749a2f001315d10af8f4
jacobmott <jacobmott@gmail.com>
2024-08-17 21:03:09 -0500

    Fixing MongoDB databse connection info

--- configs/environmentLOCAL.ts
+++ configs/environmentLOCAL.ts
@@ -11,8 +11,9 @@ export const environment = {
   // api is the name of the apio service in the docker compose file, so we can referencve it that way
   // backend is in the ngxinx config file, so we can reference it that way
   // WARNING you cant actually use the docker service name at this level, so we cant us api, we need to use localhost
-  basepath: "http://localhost:3000/backend/aquila",
-  url: "http://localhost:4200",
+  // basepath: "http://localhost:3000/aquila",
+  basepath: "http://localhost/backend/aquila",
+  url: "http://localhost",
   auth: {
     domain,
     clientId,
